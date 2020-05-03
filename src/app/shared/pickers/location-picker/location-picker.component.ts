4afab4f38dfadb8a0e56cc4cd60c337911c79151
renaonfire <renate.brook@gmail.com>
2020-05-03 15:21:26 +0100

    add select image for location

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -4,7 +4,9 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/places/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -12,6 +14,9 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./location-picker.component.scss'],
 })
 export class LocationPickerComponent implements OnInit {
+
+  selectedLocationImage: string;
+  isLoading = false; 
 
   constructor(
     private modalCtrl: ModalController,
@@ -28,9 +33,23 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(address => {
-          console.log(address);
-        });
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        }
+        this.isLoading = true;
+        this.getAddress(modalData.data.lat, modalData.data.lng)
+        .pipe(switchMap(address => {
+          pickedLocation.address = address;
+          return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14))
+        })
+        ).subscribe(staticMapImageURL => {
+          this.isLoading = false;
+          pickedLocation.staticMapImageUrl = staticMapImageURL;
+          this.selectedLocationImage = staticMapImageURL;
+        })
       });
       modalEl.present();
     });
@@ -50,6 +69,12 @@ export class LocationPickerComponent implements OnInit {
         }
         return geoData.results[0].formatted_address;
       }))
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Place%7C${lat},${lng}
+    &key=${environment.googleMapsAPI}`
   }
 
 }
